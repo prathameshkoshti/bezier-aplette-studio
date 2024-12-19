a66a0edfbb9d3f46a65a1efa1829e87bb748c8c7
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2024-12-19 23:57:37 +0530

    feat: add tooltip for free hand mode button

--- ui/components/FreeHandMode/index.tsx
+++ ui/components/FreeHandMode/index.tsx
@@ -1,7 +1,14 @@
 import Curve from '@components/Icons/Curve';
 import { ToggleGroup, ToggleGroupItem } from '@components/ui/toggle-group';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@components/ui/tooltip';
 import useColorPicker from '@store/colorPicker';
 import { useShallow } from 'zustand/react/shallow';
+import styles from './freeHandMode.module.css';
 
 function FreeHandMode() {
   const { freeHandMode, updateFreeHandMode } = useColorPicker(
@@ -18,11 +25,27 @@ function FreeHandMode() {
 
   return (
     <div className="flex items-center justify-end gap-2 py-2">
-      <ToggleGroup type="single" onClick={handleFreeHandMode}>
-        <ToggleGroupItem value="freeHandMode" aria-label="Free Hand Mode">
-          <Curve />
-        </ToggleGroupItem>
-      </ToggleGroup>
+      <TooltipProvider delayDuration={1}>
+        <Tooltip>
+          <TooltipTrigger>
+            <ToggleGroup
+              type="single"
+              variant="outline"
+              defaultValue="freeHandMode"
+              onClick={handleFreeHandMode}
+            >
+              <ToggleGroupItem
+                className={styles.toggleGroupItem}
+                value="freeHandMode"
+                aria-label="Free Hand Mode"
+              >
+                <Curve stroke={freeHandMode ? '#fff' : '#000'} />
+              </ToggleGroupItem>
+            </ToggleGroup>
+          </TooltipTrigger>
+          <TooltipContent>Toggle Free Hand Mode</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 }
