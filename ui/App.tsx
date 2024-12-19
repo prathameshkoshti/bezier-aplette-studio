369b7952b78cbf5e991f21638b1e95d32e05a2b4
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2024-12-19 20:36:07 +0530

    feat: render swatch component

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,11 +1,13 @@
 import ColorPickerInput from '@components/ColorPickerInput';
 import './reset.css';
 import './App.css';
+import Swatches from '@components/Swatches';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container flex">
       <ColorPickerInput />
+      <Swatches />
     </div>
   );
 }
