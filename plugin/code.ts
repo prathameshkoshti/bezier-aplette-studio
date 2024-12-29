b78137971f62739440df39ecad266e3efb37ca57
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2024-12-29 19:55:40 +0530

    feat: notify on export swatch and create styles

--- plugin/code.ts
+++ plugin/code.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
 import type { SwatchData } from './types';
 import {
   BORDER_RADIUS,
@@ -18,11 +16,6 @@ import {
   getFillColor,
   preloadFonts,
 } from './utils';
-
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // Load commonly used fonts when plugin starts
 (async () => {
@@ -46,6 +39,9 @@ const createStyles = (swatches: SwatchData[]) => {
           style.paints = getFillColor(r, g, b);
         }
       }
+      if (swatches.length) {
+        figma.notify('Styles Created! 🎉');
+      }
     } catch (error) {
       console.error('Error in exporting styles:', error);
       figma.notify('Error occurred while exporting files!', { error: true });
@@ -55,7 +51,6 @@ const createStyles = (swatches: SwatchData[]) => {
 
 const createPalette = (swatches: SwatchData[]) => {
   try {
-    const nodes: SceneNode[] = [];
     const swatchCollection = createAutoLayout(
       SWATCH_COLLECTION_PADDING,
       SWATCH_COLLECTION_ITEMS_SPACING,
@@ -72,8 +67,6 @@ const createPalette = (swatches: SwatchData[]) => {
         SWATCH_ITEMS_SPACING,
         'VERTICAL',
       );
-      // frame.x = 0;
-      // frame.y = 0;
       figma.currentPage.appendChild(frame);
       frame.cornerRadius = BORDER_RADIUS;
 
@@ -94,7 +87,10 @@ const createPalette = (swatches: SwatchData[]) => {
       frame.appendChild(swatchFrameGroup);
       swatchCollection.appendChild(frame);
     }
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    if (swatches.length) {
+      figma.notify('Palette Exported! 🎉');
+    }
+    figma.viewport.scrollAndZoomIntoView([swatchCollection]);
   } catch (error) {
     console.error('Error while creating palette:', error);
     figma.notify('Error occurred while creating palette!', { error: true });
@@ -108,12 +104,7 @@ figma.showUI(__html__, {
   themeColors: true,
 });
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
 figma.ui.onmessage = (msg: { type: string; colors: SwatchData[] }) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
   switch (msg.type) {
     case 'create-color-styles': {
       createStyles(msg.colors);
