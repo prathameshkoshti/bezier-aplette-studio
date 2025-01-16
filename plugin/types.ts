eb0cfdad4761df449aac19474fb7b498812b80f1
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2025-01-16 22:57:01 +0530

    feat: store and retrieve plugin data using figma apis

--- plugin/types.ts
+++ plugin/types.ts
@@ -20,3 +20,34 @@ export type SwatchData = {
   token: string;
   colors: ColorData[];
 };
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = [],
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+type Range<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type CoordinatesState = {
+  startPoint: Point;
+  endPoint: Point;
+  startPointHandle: Point;
+  endPointHandle: Point;
+};
+
+export type Swatch = CoordinatesState & {
+  id: string;
+  name: string;
+  stepCount: number;
+  hue: Range<0, 361>;
+};
