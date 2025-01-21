ab50d280dc092e97e0a3cfcd3b18f9041bc441a2
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2025-01-21 19:54:18 +0530

    feat: update accordion colors

--- ui/components/ui/accordion.tsx
+++ ui/components/ui/accordion.tsx
@@ -33,7 +33,7 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       {children}
-      <ChevronDownIcon className="h-4 w-4 shrink-0 text-zinc-500 transition-transform duration-200 dark:text-zinc-400" />
+      <ChevronDownIcon className="h-4 w-4 shrink-0 text-neutral-1000 transition-transform duration-200 dark:text-neutral-400" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
