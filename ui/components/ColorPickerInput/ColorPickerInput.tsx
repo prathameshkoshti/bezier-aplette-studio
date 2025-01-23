ad265cc18839cdb47844c6dc24593647f1f8797d
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2025-01-23 10:26:29 +0530

    style: refactor and reuse style for button toggle

--- ui/components/ColorPickerInput/ColorPickerInput.tsx
+++ ui/components/ColorPickerInput/ColorPickerInput.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import ColorPicker from '@components/ColorPicker';
 import StepCount from '@components/StepCount';
 import CurveTypeSelect from '@components/CurveType';
-import FreeHandModeCheckbox from '@components/FreeHandMode';
+import FreeHandMode from '@components/FreeHandMode';
 import GenerateSwatchButton from '@components/GenerateSwatchButton';
 import SwatchName from '@components/SwatchName';
 import PluginInfo from '@components/PluginInfo';
@@ -21,7 +21,7 @@ function ColorPickerInput() {
       </div>
       <ColorPicker />
       <div className="flex gap-4 mb-4 items-end">
-        <FreeHandModeCheckbox />
+        <FreeHandMode />
         <CurveTypeSelect />
       </div>
       <div className="flex justify-between">
