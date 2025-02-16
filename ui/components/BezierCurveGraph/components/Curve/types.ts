2df5ca1d2acfea3a1d21e65eb37e6ed90974e398
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2025-02-16 12:10:51 +0530

    feat: support quadratic curve in curve component

--- ui/components/BezierCurveGraph/components/Curve/types.ts
+++ ui/components/BezierCurveGraph/components/Curve/types.ts
@@ -1,8 +1,18 @@
 import type { Point } from '@appTypes/coords';
 
-export type CurveProps = {
+export type MandatoryCurveProps = {
   startPoint: Point;
   endPoint: Point;
+};
+
+export type CubicCurveProps = {
   startPointHandle: Point;
   endPointHandle: Point;
 };
+
+export type QuadraticCurveProps = {
+  controlPoint: Point;
+};
+
+export type CurveProps = MandatoryCurveProps &
+  (CubicCurveProps | QuadraticCurveProps);
